d6f18a88d6e8c9be72d1476253ee1d7df3647580
NRP7 <nathan.ronnie.paje@gmail.com>
2023-10-04 20:17:28 +0800

    Updated Website Loader
    I have change the looks of the website and added a preloader.

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,7 +1,18 @@
-// JavaScript to show and hide the loader
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Hide the loader when the content is fully loaded
+    // Simulate a delay for demonstration purposes (remove this line in your actual implementation)
     setTimeout(function () {
-        document.querySelector(".loader-container").style.display = "none";
+        hideLoader();
     }, 2000); // Adjust the delay as needed
+
+    function hideLoader() {
+        // Find the loader container element
+        const loaderContainer = document.querySelector(".loader-container");
+
+        // Set its display style to "none" to hide it
+        loaderContainer.style.display = "none";
+
+        // Show the scrollbars
+        document.body.style.overflow = "auto";
+    }
 });

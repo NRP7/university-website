5f5e0b0bc5b9ece1b77a50da4743926977c5b47f
NRP7 <nathan.ronnie.paje@gmail.com>
2023-10-05 19:06:44 +0800

    Added Scroll To Top Function
    I created a button the would allow the user to go back to the top of the webpage.

--- resource/js/script.js
+++ resource/js/script.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.style.overflow = "auto";
     }
 });
+
+const btnScrollToTop = document.querySelector(".btn-scroll-to-top");
+
+btnScrollToTop.addEventListener("click", function(){
+
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    });
+
+});
+
+$(document).ready(function () {
+    $('#card-carousel').carousel({
+        interval: 3000, // Set the interval between slides (in milliseconds)
+        pause: false,   // Set to true if you want to pause on hover
+        wrap: true      // Set to true for continuous looping
+    });
+});
